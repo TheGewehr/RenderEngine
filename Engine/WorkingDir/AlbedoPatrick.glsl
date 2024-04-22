8c3fb9764153fa90cae30afcaef61647da811a2c
TheGewehr <alexandre07979@gmail.com>
2024-04-22 21:08:52 +0200

    Quick checkoint

--- Engine/WorkingDir/AlbedoPatrick.glsl
+++ Engine/WorkingDir/AlbedoPatrick.glsl
@@ -5,29 +5,51 @@
 
 #if defined(VERTEX) ///////////////////////////////////////////////////
 
+struct Light
+{
+	unsigned int type;
+	vec3 color;
+	vec3 direction;
+	vec3 position;
+};
+
+layout(location = 0) in vec3 aPosition;	// world space
+layout(location = 1) in vec3 aNormal;	// world space
+layout(location = 2) in vec2 aTexCoord;
+
+layout(binding = 0, std140) uniform globalParams
+{
+	vec3 uCameraPosition;
+	unsigned int uLightCount;
+	Light uLight[16];
+};
+
 layout(binding = 1, std140) uniform localParams
 {
 	mat4 uWorldMatrix;
 	mat4 uWorldViewProjectionMatrix;
 };
 
-layout(location = 0) in vec3 aPosition;	// world space
-layout(location = 1) in vec3 aNormal;	// world space
-layout(location = 2) in vec2 aTexCoord;
-layout(location = 3) in vec3 aTangent;
-layout(location = 4) in vec3 aBitangent;
+//layout(location = 3) in vec3 aTangent;
+//layout(location = 4) in vec3 aBitangent;
 
 out vec2 vTexCoord;
 out vec3 vPosition;
 out vec3 vNormal;
-
+out vec3 vViewDir;
+out vec3 lightDir;
+out vec3 lightColor;
 
 void main()
 {
 	vTexCoord = aTexCoord;
 	vPosition = vec3(uWorldMatrix * vec4(aPosition, 1.0));
 	vNormal =	vec3(uWorldMatrix * vec4(aNormal, 0.0));
-	
+	vViewDir = uCameraPosition - vPosition;
+
+	// normalizar direccion, tener en cuenta si es point o directional, ya que estoy hacerlo para todas las luces
+	lightDir = uLight[0].direction;
+	lightColor = uLight[0].color;
 
 	gl_Position = uWorldViewProjectionMatrix * vec4(aPosition, 1.0);
 }
@@ -42,6 +64,7 @@ layout(location = 0) out vec4 oColor;
 
 void main()
 {
+	// como en shadertoy usando dot y mix 
     oColor = texture(uTexture, vTexCoord);
 }
 
